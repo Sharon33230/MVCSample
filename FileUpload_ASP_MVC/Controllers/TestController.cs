a146a2dd57f9450e450ecb38bfe7e5bc0f2f39df
Sharon Palaparthi <sharon33230@gmail.com>
2025-03-01 16:14:10 +0530

    Added new controller file

---
+++ FileUpload_ASP_MVC/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FileUpload_ASP_MVC.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
